40f0e185ce98a624caba681d30952479581200f8
Glory Agatevure <agatevureglory@gmail.com>
2023-10-20 12:59:47 +0100

    Update IFtsoRegistry.sol

--- blockchain/contracts/IFtsoRegistry.sol
+++ blockchain/contracts/IFtsoRegistry.sol
@@ -1,35 +1,32 @@
-// SPDX-License-Identifier: MIT
-interface IFtsoRegistry { 
+// IFtsoRegistry Solidity API Interface
+interface IFtsoRegistry {
+    // Returns the list of supported symbols like BTC, ETH, FLR...
+    // @return _supportedSymbols An array of supported symbols
+    // @dev This function doesn't revert.
 
-    // Returns the list of supported symbols like BTC, ETH, FLR... 
-    /**
-     * @dev Returns `_supportedSymbols`, An array of strings representing the supported symbols.
-     */
-    function getSupportedSymbols() 
-        external 
-        view 
-        returns( 
-            string[] memory _supportedSymbols
-        ); 
+    function getSupportedSymbols() external view returns (string[] memory _supportedSymbols);
 
     // Returns USD price of the requested symbol, which must be divided by
-    // the returned number of decimals, e.g., if decimals is 5, a price of 
+    // the returned number of decimals, e.g., if decimals is 5, a price of
     // 1234 actually means 0.01234
-
-    /**
-     * @dev Sets `_symbol` A string representing the symbol for which the price is requested (e.g., "BTC" for Bitcoin).
-     * Returns `_price` An unsigned integer representing the current price of the symbol, 
-     * `_timestamp` An unsigned integer representing the timestamp of the price data and
-     *  `_assetPriceUsdDecimals` An unsigned integer specifying the number of decimals to divide _price to get the actual USD value
-     */
-    function getCurrentPriceWithDecimals(string memory _symbol) 
-        external view 
-        returns( 
-            uint256 _price, 
-            uint256 _timestamp, 
+    // @param _symbol The symbol of the asset to query, e.g., "BTC" or "ETH"
+    // @return _price The USD price of the requested symbol
+    // @return _timestamp The timestamp when the price information was recorded
+    // @return _assetPriceUsdDecimals The number of decimals for the asset's price
+    // @dev This function doesn't revert.
+    // @dev The `_timestamp` is in Unix timestamp format (seconds since epoch).
+    // @dev There are no special values for the parameters.
+    function getCurrentPriceWithDecimals(string memory _symbol)
+        external
+        view
+        returns (
+            uint256 _price,
+            uint256 _timestamp,
             uint256 _assetPriceUsdDecimals
-        ); 
-} 
+        );
+}
 
 
-//
+
+
+
